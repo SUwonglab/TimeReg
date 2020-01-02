eb4f4bd0ea3d6cde83924299618cfd4e93de2d00
Zhana Duren <36247822+durenzn@users.noreply.github.com>
2020-01-01 18:58:08 -0800

    Update tcPECA.sh

--- tcPECA.sh
+++ tcPECA.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 # Time Course Regulatory Analysis(TCRA) updated Aug 14th 2019
-
+mkdir ./exampleData/Out
 cat TCRA_config TCRA.m > run_TCRA.m
 module load matlab
 matlab -nodisplay -nosplash -nodesktop -r "run_TCRA; exit"
