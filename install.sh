ea45d3bb4709b2716d77931fe6b0e446ce9a12dc
Zhana Duren <36247822+durenzn@users.noreply.github.com>
2023-11-10 17:21:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 #/bin/bash
 
 #download the prior data 
-wget http://web.stanford.edu/~zduren/PECA/Thresholding-Based%20SVD_files/Prior_corr.tar.gz
+wget https://web.stanford.edu/group/wonglab/zduren/PECA/Thresholding-Based%20SVD_files/Prior_corr.tar.gz
 tar -zxvf Prior_corr.tar.gz
 rm Prior_corr.tar.gz
